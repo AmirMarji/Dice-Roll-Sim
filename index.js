851483ec08a4db36ef18fe15a7b788682c84c457
Amir E Marji <98343618+AmirMarji@users.noreply.github.com>
2025-08-02 23:30:12 -0400

    finish tut

--- index.js
+++ index.js
@@ -2,11 +2,55 @@ console.log("Javascript loaded ...");
 
 const rollDiceButton = document.getElementById("roll-button");
 const mainDice = document.getElementById("dice");
+const rollHistory = document.getElementById("roll-history");
 
-rollDiceButton.addEventListener("click", ()=>{
+let historyList = [];
+
+function rollDice() {
+    const rollResult = Math.floor(Math.random() * 6) + 1;
+    const diceFace = getDiceFace(rollResult);
+    mainDice.innerHTML = diceFace;
+    historyList.push(rollResult);
+    updateRollHistory();
+}
+
+function updateRollHistory() {
+    rollHistory.innerHTML = "";
+    for (let i = 0; i < historyList.length; i++) {
+        const listItem = document.createElement("li");
+        listItem.innerHTML = `Roll ${i + 1}: <span>${getDiceFace(
+            historyList[i]
+        )}</span>`;
+        rollHistory.appendChild(listItem);
+    }
+}
+
+function getDiceFace(rollResult) {
+
+    switch (rollResult) {
+        case 1:
+            return "&#9856;";
+        case 2:
+            return "&#9857;";
+        case 3:
+            return "&#9858;";
+        case 4:
+            return "&#9859;";
+        case 5:
+            return "&#9860;";
+        case 6:
+            return "&#9861;";
+        default:
+            return "";
+    }
+
+}
+
+rollDiceButton.addEventListener("click", () => {
     mainDice.classList.add("roll-animation");
     // 1 sec
-    setTimeout(()=>{
+    setTimeout(() => {
         mainDice.classList.remove("roll-animation");
-    },1000);
+        rollDice();
+    }, 1000);
 });
