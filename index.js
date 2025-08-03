99f9a1acb0fc1f7f18e58607ac2645763b521010
Amir E Marji <98343618+AmirMarji@users.noreply.github.com>
2025-08-02 22:32:37 -0400

    we got the button to click!

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 console.log("Javascript loaded ...");
+
+const rollDiceButton = document.getElementById("roll-button");
+
+rollDiceButton.addEventListener("click", ()=>{
+    console.log("The button was clicked");
+});
