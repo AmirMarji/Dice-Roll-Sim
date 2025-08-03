64f131177adda8b64df46294119f5fd3fcbd9744
Amir E Marji <98343618+AmirMarji@users.noreply.github.com>
2025-08-02 22:58:48 -0400

    animation for 1 dice

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 console.log("Javascript loaded ...");
 
 const rollDiceButton = document.getElementById("roll-button");
+const mainDice = document.getElementById("dice");
 
 rollDiceButton.addEventListener("click", ()=>{
-    console.log("The button was clicked");
+    mainDice.classList.add("roll-animation");
+    // 1 sec
+    setTimeout(()=>{
+        mainDice.classList.remove("roll-animation");
+    },1000);
 });
